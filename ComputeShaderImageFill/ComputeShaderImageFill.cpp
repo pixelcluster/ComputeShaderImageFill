bfae7cccd0406dccf616fd019fc096a5e55f3686
pixelcluster <83122513+pixelcluster@users.noreply.github.com>
2021-09-02 23:29:50 +0200

    Add project files.

---
+++ ComputeShaderImageFill/ComputeShaderImageFill.cpp
@@ -0,0 +1,12 @@
+﻿// ComputeShaderImageFill.cpp : Defines the entry point for the application.
+//
+
+#include "ComputeShaderImageFill.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
